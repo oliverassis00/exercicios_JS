b86f89e31710b66b0870b747dbb7283ef6aaaa7c
Oliver Assis <oliverinvest08@gmail.com>
2025-06-16 22:53:33 -0300

    Exercicios ok

---
+++ exercicios_fase4/exercicios_reais.js
@@ -0,0 +1,59 @@
+// const prompt = require('prompt-sync') ();
+
+// let nome = "Oliver"
+// let idade = 25
+// let cidade = "Aparecida do Taboaba"
+
+
+// console.log(`Meu nome é ${nome}, tenho ${idade} e moro em ${cidade}.`)
+
+// let numero1 = 10;
+// let numero2 = 5;
+// let resultado = numero1 + numero2;
+
+// console.log(`O resultado é ${resultado}.`);
+
+// const prompt = require('prompt-sync') ();
+
+// let digiteumNumero = Number(prompt("Digite um número: "));
+
+// if (digiteumNumero % 2 == 0) {
+//     console.log("O número é par.")
+// } else {
+//     console.log("O número é ímpar.")
+// }
+
+
+// function calculaMedia (primeiraNota, segundaNota, terceiraNota) {
+//     let mediaAluno = (primeiraNota + segundaNota + terceiraNota) / 3
+
+//     return mediaAluno >= 7 ? `Aprovado` : `Reprovado`
+
+// }
+
+// console.log(calculaMedia (5, 4, 3));
+
+
+// for(let c = 10; c >= 1; c --) {
+//     console.log(c);
+// }
+
+// const prompt = require('prompt-sync') ();
+
+// let digiteNumero = parseInt(prompt("Digite um número de 1 a 10: "));
+
+// for (let i = 0; i <= 10; i ++) {
+//     console.log(`${digiteNumero} x ${i} = ${digiteNumero * i}`);
+// }
+
+// function saudar(nome) {
+//     console.log(`Olá, ${nome}! Seja bem-vindo (a)!`)
+// }
+
+// saudar("Oliver");
+
+// function ehMultiplo (num, mult) {
+//     return num % mult == 0 ? true : false;
+// }
+
+// console.log(ehMultiplo(5,2));
